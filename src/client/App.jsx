68da60c8ed70099eaf897e93d8e70c15278bde0b
Blake <bgeard@wgu.edu>
2024-01-12 13:47:52 -0500

    creates empty matches and profile files and adds routes

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import LandingPage from "./components/LandingPage";
 import ChatRoom from "./components/ChatRoom";
-
+import Profile from "./components/Profile";
+import Matches from "./components/Matches";
 const App = () => {
   return (
     <Router>
@@ -10,6 +11,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/chat" element={<ChatRoom />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/matches" element={<Matches />} />
       </Routes>
     </Router>
   );
