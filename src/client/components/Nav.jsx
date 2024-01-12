d71abb3820358fd07dda26eb2ea16100e45b47b1
Joe Philavanh <joecphilavanh@yahoo.com>
2024-01-12 09:19:41 -0800

    updated navbar

--- src/client/components/Nav.jsx
+++ src/client/components/Nav.jsx
@@ -2,27 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import "../styles/Nav.css";
 
-
-
 function Navbar() {
     return (
         <nav className="navbar">
-            <ul>
-                <li>
-                    <Link to="/">
-                        logo
-                    </Link>
-                </li>
-            </ul>
-            <ul className="navbar-right">
-                <li><Link to="/profile" className="navbar-link">Profile Creation</Link></li>
-                <li><Link to="/notifications" className="navbar-link">Notifications</Link></li>
-                <li><Link to="/matches" className="navbar-link">Matches</Link></li>
-                <li><Link to="/logout" className="navbar-link">Logout</Link></li>
-            </ul>
+            <div className="navbar-left">
+                <Link to="/">logo</Link>
+            </div>
+            <div className="navbar-right">
+                <Link to="/profile" className="navbar-link">Profile Creation</Link>
+                <Link to="/notifications" className="navbar-link">Notifications</Link>
+                <Link to="/matches" className="navbar-link">Matches</Link>
+                <Link to="/logout" className="navbar-link">Logout</Link>
+            </div>
         </nav>
     );
 }
 export default Navbar;
-
-
