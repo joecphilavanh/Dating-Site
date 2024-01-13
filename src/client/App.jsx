691a34c8114dd590db7208dc4863bba4aeeaf791
Blake <bgeard@wgu.edu>
2024-01-13 06:18:06 -0500

    sets up register and login endpoints/logic

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,22 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import LandingPage from "./components/LandingPage";
+import Register from "./components/ Register";
 import ChatRoom from "./components/ChatRoom";
 import Profile from "./components/Profile";
 import Matches from "./components/Matches";
 import Login from "./components/Login";
+import {AuthProvider} from "./context/AuthContext";
+
 const App = () => {
   return (
+  <AuthProvider>
     <Router>
-             <Nav />
+        <Nav />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/chat" element={<ChatRoom />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/matches" element={<Matches />} />
         <Route path="/login" element={<Login />} />
+        <Route path={"/register"} element={<Register />} />
       </Routes>
     </Router>
+  </AuthProvider>
   );
-}
+};
 export default App;
--- src/server/main.js
+++ src/server/main.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const ViteExpress = require("vite-express");
-
+const authRoutes = require("./routes/auth.js");
+const profileRoutes = require("./routes/profileRoutes.js");
 const app = express();
 const cors = require('cors');
 app.use(cors());
+app.use(express.json());
 
-app.get("/hello", (req, res) => {
-  res.send("Hello Vite + React!");
-});
-
+app.use("/auth", authRoutes);
+app.use("/profile", profileRoutes);
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
 );
