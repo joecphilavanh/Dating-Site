cd9c8b889c18539b4fddb502a7293a4496b1be3a
Carlos Silva <silvacj90@gmail.com>
2024-01-16 23:20:04 -0500

    Build out skeleton for profile page

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,31 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import LandingPage from "./components/LandingPage";
-import Register from "./components/ Register";
+import Register from "./components/Register";
 import ChatRoom from "./components/ChatRoom";
 import ProfileCreation from "./components/ProfileCreation";
 import Profile from "./components/Profile";
 import Matches from "./components/Matches";
 import Login from "./components/Login";
-import {AuthProvider} from "./context/AuthContext";
-
+import { AuthProvider } from "./context/AuthContext";
 
 const App = () => {
   return (
-      <AuthProvider>
-        <Router>
-          <Nav /> {/* Render the Nav component at the top level */}
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/chat" element={<ChatRoom />} />
-            <Route path="/createprofile" element={<ProfileCreation />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/matches" element={<Matches />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </Router>
-      </AuthProvider>
+    <AuthProvider>
+      <Router>
+        <Nav /> {/* Render the Nav component at the top level */}
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/chat" element={<ChatRoom />} />
+          <Route path="/createprofile" element={<ProfileCreation />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/matches" element={<Matches />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
