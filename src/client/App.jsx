ecb5e27497ab393b820bbd6a58b0fbd6ef053136
Blake G <143160866+engineergrowth@users.noreply.github.com>
2024-01-18 12:07:57 -0500

    Revert "intial commit for new branch"

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -7,29 +7,25 @@ import ProfileCreation from "./components/ProfileCreation";
 import Profile from "./components/Profile";
 import Matches from "./components/Matches";
 import Login from "./components/Login";
-import DiscoveringPeople from "./components/DiscoveringPeople";
-import {AuthProvider} from "./context/AuthContext";
-
+import { AuthProvider } from "./context/AuthContext";
 
 
 const App = () => {
   return (
-      <AuthProvider>
-        <Router>
-          <Nav /> {/* Render the Nav component at the top level */}
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/chat" element={<ChatRoom />} />
-            <Route path="/createprofile" element={<ProfileCreation />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/matches" element={<Matches />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/DiscoveringPeople" element={<DiscoveringPeople />} />
-
-          </Routes>
-        </Router>
-      </AuthProvider>
+    <AuthProvider>
+      <Router>
+        <Nav /> {/* Render the Nav component at the top level */}
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/chat" element={<ChatRoom />} />
+          <Route path="/createprofile" element={<ProfileCreation />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/matches" element={<Matches />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
