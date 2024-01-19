3b5dcefa86b4da108748f058cd32e2bf70a60631
Blake <bgeard@wgu.edu>
2024-01-19 15:18:46 -0500

    fixes server bugs with directory paths

--- src/server/main.js
+++ src/server/main.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const ViteExpress = require("vite-express");
-const authRoutes = require("./routes/auth.js");
-const profileRoutes = require("./routes/profileRoutes.js");
+const authRoutes = require("./api/routes/auth.js");
+const profileRoutes = require("./api/routes/profileRoutes.js");
 const app = express();
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-app.use("/auth", authRoutes);
-app.use("/profile", profileRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/profile", profileRoutes);
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000...")
 );
