5ab8965d703f7166995fd34de09e92d17a969366
Blake <bgeard@wgu.edu>
2024-01-24 14:34:05 -0500

    removes chat from react router

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import LandingPage from "./components/LandingPage";
 import Register from "./components/Register";
-import ChatRoom from "./components/ChatRoom";
+// import ChatRoom from "./components/ChatRoom";
 import ProfileCreation from "./components/ProfileCreation";
 import Profile from "./components/Profile";
 import Matches from "./components/Matches";
@@ -20,7 +20,7 @@ const App = () => {
         <Nav /> {/* Render the Nav component at the top level */}
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/chat" element={<ChatRoom />} />
+          {/*<Route path="/chat" element={<ChatRoom />} />*/}
           <Route path="/createprofile" element={<ProfileCreation />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/matches" element={<Matches />} />
