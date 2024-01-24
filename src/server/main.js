e95b8711d5f67b1d5ff87dd6fd9f77ac1a95987d
Blake <bgeard@wgu.edu>
2024-01-24 13:53:49 -0500

    adds backend routes for messaging and sets it up in server file

--- src/server/main.js
+++ src/server/main.js
@@ -4,6 +4,7 @@ const ViteExpress = require("vite-express");
 const authRoutes = require("./api/routes/auth.js");
 const profileRoutes = require("./api/routes/profileRoutes.js");
 const likeRoutes = require("./api/routes/likeRoutes.js");
+const messageRoutes = require("./api/routes/messageRoutes.js");
 const cors = require("cors");
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/profile", profileRoutes);
 app.use("/api/like", likeRoutes);
+app.use("/api/message", messageRoutes);
 ViteExpress.listen(app, 3000, () =>
     console.log("Server is listening on port 3000...")
 );
